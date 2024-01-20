deb9313854aa44443723c6c4e90c05a7766bae06
Marcio122 <marcio122@outlook.com.br>
2024-01-20 15:23:05 +0100

    If statement

---
+++ if_statement.py
@@ -0,0 +1,17 @@
+light = "Red"
+
+if light == "Red":
+    print("Stop")
+else:
+    print("Go")
+    
+
+x = 10
+
+if x >= 0:
+    print("Positive")
+
+else:
+    print("Negative")
+    
+print("Done")
